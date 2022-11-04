eaae38a21c8fd556618129cedd11976d7064604f
jhorology <jhorology2014@gmail.com>
2022-11-05 00:27:09 +0900

    add local build scripts

---
+++ build.container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT=$(dirname $(realpath $0))
+cd $PROJECT
+
+if [ ! -d .west/ ]; then
+    west init -l config
+    UPDATE_BUILD_ENV=true
+fi
+
+if $UPDATE_BUILD_ENV; then
+    west update
+    west zephyr-export
+fi
+west build -s zmk/app -b bt60 -- -DZMK_CONFIG=/workdir/config
+
---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/zsh -eu
+
+PROJECT=$(realpath $0:h)
+cd "$PROJECT"
+
+zparseopts -D -E -F -- \
+           {h,-help}=help  \
+           -without-update=without_update \
+    || return
+
+
+if (( $#help )); then
+    print -rC1 --      \
+          "Usage:" \
+          "$0:t <-h|--help>                                            help" \
+          "$0:t [options...]                                           build firmware" \
+          "" \
+          "options:" \
+          "  -w,--without-update              don't sync remote repository"
+    return
+fi
+
+# configuration
+# -----------------------------------
+DOCKER_IMAGE=zmkfirmware/zmk-dev-arm:stable
+UPDATE_BUILD_ENV=true
+
+#  override configuration
+# -----------------------------------
+[ -s .config ] &&  source .config
+
+# option parameters
+# -----------------------------------
+(( $#without_update )) && UPDATE_BUILD_ENV=false
+
+# pull docker  image
+# -----------------------------------
+[ -z "$(docker image ls | grep $DOCKER_IMAGE)" ] && \
+    docker pull $DOCKER_IMAGE
+
+# setup build env
+# -----------------------------------
+if $UPDATE_BUILD_ENV; then
+    # revert changes
+    pushd zmk
+    git reset --hard HEAD
+    git clean -dfx
+    popd
+fi
+
+docker run -it --rm -v $PROJECT:/workdir --env UPDATE_BUILD_ENV=$UPDATE_BUILD_ENV --name zmk-build $DOCKER_IMAGE /workdir/build.container.sh
+
+pushd zmk
+VERSION="$(date +"%Y%m%d")_zmk_$(git rev-parse --short HEAD)"
+popd
+
+mkdir dist
+cp build/zephyr/zmk.uf2 dist/$(git branch --show-current)_${VERSION}.uf2
